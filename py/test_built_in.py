692ea840f3ad989fce041e5f75d8781c1cd7e271
Mohit Singh Chouhan <mohitchouhan1947@gmail.com>
2020-07-22 00:13:42 +0530

    Add test_built_in.py and linear_search.py

---
+++ py/test_built_in.py
@@ -0,0 +1,11 @@
+ishwar_stack = []
+
+for i in range(10):
+    ishwar_stack.append(i)  # pushing into the stack
+
+print(ishwar_stack)
+
+ishwar_stack.pop()
+ishwar_stack.pop()
+
+print(ishwar_stack)
